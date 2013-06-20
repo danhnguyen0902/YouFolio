2fbc59bfc23cb5e9934744053858da08b9be3b58
Robert Wagner <rwwagner@vt.edu>
2013-06-20 19:52:52 -0400

    Fixed positioning of portfolio share, favorites, etc

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -2,40 +2,49 @@ function portfolioController($scope) {
 	$scope.row1 = [{
 		"image": "img/content.png",
 		"title": "Row 1",
-		"date": "16 June 2013"
+		"date": "16 June 2013",
+		"tags": "tag1, tag2",
 	}, {
 		"image": "img/content.png",
 		"title": "Row 1",
-		"date": "16 June 2013"
+		"date": "16 June 2013",
+		"tags": "tag1, tag2",
 	}, {
 		"image": "img/content.png",
 		"title": "Row 1",
-		"date": "16 June 2013"
+		"date": "16 June 2013",
+		"tags": "tag1, tag2",
 	}, ];
 	$scope.row2 = [{
 		"image": "img/content.png",
 		"title": "Row 2",
-		"date": "16 June 2013"
+		"date": "16 June 2013",
+		"tags": "tag1, tag2",
 	}, {
 		"image": "img/content.png",
 		"title": "Row 2",
-		"date": "16 June 2013"
+		"date": "16 June 2013",
+		"tags": "tag1, tag2",
 	}, {
 		"image": "img/content.png",
 		"title": "Row 2",
-		"date": "16 June 2013"
+		"date": "16 June 2013",
+		"tags": "tag1, tag2",
 	}, ];
 	$scope.row3 = [{
 		"image": "img/content.png",
 		"title": "Row 3",
-		"date": "16 June 2013"
+		"date": "16 June 2013",
+		"tags": "tag1, tag2",
 	}, {
 		"image": "img/content.png",
 		"title": "Row 3",
-		"date": "16 June 2013"
+		"date": "16 June 2013",
+		"tags": "tag1, tag2",
 	}, {
 		"image": "img/content.png",
 		"title": "Row 3",
-		"date": "16 June 2013"
+		"date": "16 June 2013",
+		"tags": "tag1, tag2",
 	}, ];
 }
