c45161e4bf7c5fd61479f390777e4bc7c1b39f36
Robert Wagner <rwwagner@vt.edu>
2013-06-20 16:31:24 -0400

    Added in some rough angular stuff

---
+++ static/js/controllers.js
@@ -0,0 +1,41 @@
+function portfolioController($scope) {
+	$scope.row1 = [{
+		"image": "img/content.png",
+		"title": "Row 1",
+		"date": "16 June 2013"
+	}, {
+		"image": "img/content.png",
+		"title": "Row 1",
+		"date": "16 June 2013"
+	}, {
+		"image": "img/content.png",
+		"title": "Row 1",
+		"date": "16 June 2013"
+	}, ];
+	$scope.row2 = [{
+		"image": "img/content.png",
+		"title": "Row 2",
+		"date": "16 June 2013"
+	}, {
+		"image": "img/content.png",
+		"title": "Row 2",
+		"date": "16 June 2013"
+	}, {
+		"image": "img/content.png",
+		"title": "Row 2",
+		"date": "16 June 2013"
+	}, ];
+	$scope.row3 = [{
+		"image": "img/content.png",
+		"title": "Row 3",
+		"date": "16 June 2013"
+	}, {
+		"image": "img/content.png",
+		"title": "Row 3",
+		"date": "16 June 2013"
+	}, {
+		"image": "img/content.png",
+		"title": "Row 3",
+		"date": "16 June 2013"
+	}, ];
+}
