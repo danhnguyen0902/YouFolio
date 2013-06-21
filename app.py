1a27027b792b4cc904dd60041b84b66124f608fe
Michael Shuffett <mshuffett@gmail.com>
2013-06-20 18:53:26 -0700

    Added some uploading code

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
-import datetime
 
 sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '../../')))
 
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for, flash, send_from_directory
+from werkzeug import secure_filename
+from flask.views import MethodView
+from flask.ext.mongoengine import MongoEngine
+from flask_debugtoolbar import DebugToolbarExtension
 from flask.ext.login import (LoginManager, current_user, login_required,
                             login_user, logout_user, UserMixin, AnonymousUser,
                             confirm_login, fresh_login_required)
 
-
-from flask.ext.mongoengine import MongoEngine
-from flask_debugtoolbar import DebugToolbarExtension
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__, static_url_path='')
 app.config.from_object(__name__)
 app.config['MONGODB_SETTINGS'] = {'DB': 'testing'}
 app.config['TESTING'] = True
 app.config['SECRET_KEY'] = 'flask+mongoengine=<3'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.debug = True
 app.config['DEBUG_TB_PANELS'] = (
     'flask.ext.debugtoolbar.panels.versions.VersionDebugPanel',
@@ -44,6 +47,7 @@ class User(db.Document):
     email = db.EmailField(required=True, max_length=100)
     first_name = db.StringField(max_length=50, min_length=2)
     last_name = db.StringField(max_length=50, min_length=2)
+    files = db.ListField(db.GridFSProxy)
 
     def __str__(self):
         return str(self.username)
@@ -57,33 +61,97 @@ def index():
     return app.send_static_file('index.html')
 
 
-@app.route('/user')
-def get_users():
-    return User.objects().to_json()
+class UserAPI(MethodView):
+    def get(self, username):
+        if username is None:
+            # return a list of users
+            return User.objects().to_json()
+        else:
+            return User.objects(username=username).to_json()
+
+    def post(self):
+        username = request.form.get('username', None)
+        password = request.form.get('password', None)
+        email = request.form.get('email', None)
+        password = request.form.get('password', None)
+        first_name = request.form.get('firstName', None)
+        last_name = request.form.get('lastName', None)
+
+        new_user = User(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+
+        try:
+            new_user.save()
+        except Exception, e:
+            return ('Invalid Input: %s' % e.message, 400, None)
+        else:
+            return 'Success'
+
+    def delete(self, username):
+        user = User.objects(username=username)
+        if not user:
+            return ("User does not exist", 400, None)
+        user.delete()
+        return "Success"
+
+    def put(self, username):
+        user = User.objects(username=username)
+        if not user:
+            return ("User does not exist", 400, None)
+
+        for field_name in user._fields:
+            if field_name in request.form:
+                user.__setattr__(field_name, request.form[field_name])
+
+        try:
+            new_user.save()
+        except Exception, e:
+            return ('Invalid Input: %s' % e.message, 400, None)
+        else:
+            return 'Success'
+
+user_view = UserAPI.as_view('user_api')
+app.add_url_rule('/user/', defaults={'username': None},
+                 view_func=user_view, methods=['GET', ])
+app.add_url_rule('/user/', view_func=user_view, methods=['POST', ])
+app.add_url_rule('/user/<username>', view_func=user_view,
+                 methods=['GET', 'PUT', 'DELETE'])
 
 
-@app.route('/user/create', methods=['POST'])
-def create_user():
-    # return make_response(render_template('not_found.html'), 404)
-    username = request.form.get('username', False)
-    password = request.form.get('password', False)
-    email = request.form.get('email', False)
-    password = request.form.get('password', False)
-    first_name = request.form.get('firstName', False)
-    last_name = request.form.get('lastName', False)
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-    new_user = User(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
-    try:
-        new_user.save()
-    except Exception, e:
-        return ('Invalid Input: %s' % e.message, 400, None)
-    else:
-        return 'Success'
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        app.logger.debug('first')
+        app.logger.info('%s %s %s' % (request.files, request.args, request.form))
+        file = request.files['file']
+        app.logger.info('here')
+        app.logger.info(file)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4000)
-    try:
-        pass
-    except Exception, e:
-        raise e
