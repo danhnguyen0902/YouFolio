67c08cb353ac8a4b2bcb035b8725514367648f8c
Robert Wagner <rwwagner@vt.edu>
2013-06-26 22:29:59 -0400

    Added a few things to the jobs page

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -56,3 +56,23 @@ function portfolioController($scope) {
 	$scope.email = "email@website.com";
 	$scope.skype = "skypeusername";
 }
+
+function jobsController($scope) {
+$scope.jobs = [{
+		"title": "Junior Graphic Design/Intern",
+		"company": "Google Analytics",
+		"date": "12 May 2013",
+		"location": "Austin, TX",
+	}, {
+		"title": "Product Engineer/Specialist",
+		"company": "Company Name Goes Here",
+		"date": "16 June 2013",
+		"location": "The Middle of Nowhere, VA",
+	}, {
+		"title": "Job",
+		"company": "YouFolio",
+		"date": "16 June 2013",
+		"location": "Everywhere, USA",
+	}, ];
+
+}
