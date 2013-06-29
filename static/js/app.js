8701b1cabdba614d100b7afd7d3d78e52f5eadfc
Flinn <flinn118@gmail.com>
2013-06-28 23:22:57 -0400

    back to bootstrap.

--- static/js/app.js
+++ static/js/app.js
@@ -1,30 +1,22 @@
-/*global angular */
-
-/* @type {angular.Module} */
-app = angular.module("youfolio", ['ui.bootstrap']).config(function($routeProvider, $dialogProvider) {
-  $routeProvider.when('/welcome', {
+app = angular.module("youfolio", []).config(function($routeProvider) {
+  $routeProvider.when('/', {
     templateUrl: 'welcome.html', 
     controller: 'HomeCtrl' 
   });
-  $routeProvider.otherwise({ redirectTo: '/welcome' })
-
-  $dialogProvider.options({ backdropFade: true, dialogFade: true });
+  $routeProvider.otherwise({ redirectTo: '/' })
 });
 
-app.controller('HomeCtrl', function($scope) {
+app.controller('HomeCtrl', function($scope) {  
+});
+
+var SignupController = function ($scope) {
+  $scope.signupBox = {};
   
-});
+  $scope.update = function(signup) {
+    $scope.signupBox = angular.copy(signup);
+  }
 
-var SignUpCtrl = function ($scope) {
-  $scope.open = function () {
-    $scope.shouldBeOpen = true;
-  };
-  $scope.close = function () {
-    $scope.closeMsg = 'I was closed at: ' + new Date();
-    $scope.shouldBeOpen = false;
-  };  
-  $scope.opts = {
-    backdropFade: true,
-    dialogFade:true, 
-  };
+  $scope.isUnchanged = function(signup) {
+    return angular.equals(signup, $scope.master);
+  }
 };
