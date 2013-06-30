cf9f6195a8516018ac81eed9702c95b9ef484b6d
Matt Flinn <mflinn@fool.com>
2013-06-30 03:07:35 -0400

    I hate vim

--- static/js/app.js
+++ static/js/app.js
@@ -1,26 +1,26 @@
-/*global angular */
-
-/* @type {angular.Module} */
 app = angular.module("youfolio", ['ui.bootstrap']).config(function($routeProvider, $dialogProvider) {
-  $routeProvider.when('/welcome', {
-    templateUrl: 'welcome.html', 
-    controller: 'HomeCtrl' 
+  $routeProvider.when('/', {
+    templateUrl: 'pages/home.html', 
+    controller: 'HomeController' 
   });
-  $routeProvider.otherwise({ redirectTo: '/welcome' })
+  $routeProvider.otherwise({ redirectTo: '/' });
 
   $dialogProvider.options({ backdropFade: true, dialogFade: true });
 });
 
-app.controller('HomeCtrl', function($scope) {
-  
+app.controller('HomeController', function($scope) {  
 });
 
-var SignUpCtrl = function ($scope) {
+var IntroCarouselController = function($scope) {
+  $scope.introInterval = 7000;
+  $scope.slides = [{css:'firstItem'},{css:'secondItem'},{css:'thirdItem'}];
+};
+
+var SignUpController = function ($scope) {
   $scope.open = function () {
     $scope.shouldBeOpen = true;
   };
   $scope.close = function () {
-    $scope.closeMsg = 'I was closed at: ' + new Date();
     $scope.shouldBeOpen = false;
   };  
   $scope.opts = {
