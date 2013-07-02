bbdcae1541236db4b592581f67ceb51138083608
Robert Wagner <rwwagner@vt.edu>
2013-07-02 16:50:53 -0400

    Moved all scripts to resources.js and used head.js

---
+++ static/resources.js
@@ -0,0 +1,18 @@
+head.js(
+
+    /*External Libraries */
+    "http://code.jquery.com/jquery-1.10.1.min.js",
+    "http://ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular.js",
+    "http://angular-ui.github.io/bootstrap/ui-bootstrap-tpls-0.4.0.js",
+    /* Main app */
+    "js/app.js",
+    /* Controllers
+    * Location: js/controllers
+    * */
+    "js/controllers/gallery.js",
+    "js/controllers/help.js",
+    "js/controllers/home.js",
+    "js/controllers/jobs.js",
+    "js/controllers/portfolio.js",
+    "js/controllers/team.js"
+);
