f54036618ab54398cb25f7d173136756075d9594
Matt Flinn <mflinn@fool.com>
2013-07-08 21:28:06 -0400

    Modal dialog changes...

--- static/js/controllers/portfolio.js
+++ static/js/controllers/portfolio.js
@@ -27,7 +27,7 @@ app.controller('PortfolioController', [ '$dialog', '$scope', function($dialog, $
     $scope.phone = "(867)-555-5309";
     $scope.email = "email@website.com";
     $scope.skype = "skypeusername";    
-
+    
     $scope.openContentEditor = function(item) {
          var dialogOpts = {
                 templateUrl: 'partials/dialogs/content-detail.html',
@@ -42,81 +42,6 @@ app.controller('PortfolioController', [ '$dialog', '$scope', function($dialog, $
     };
 }]);
 
-// function PortfolioController($dialog, $scope) {
-//     $scope.row1 = [
-//         {
-//             "image": "img/content.png",
-//             "title": "Row 1",
-//             "date": "16 June 2013",
-//             "tags": "tag1, tag2",
-//         },
-//         {
-//             "image": "img/content.png",
-//             "title": "Row 1",
-//             "date": "16 June 2013",
-//             "tags": "tag1, tag2",
-//         },
-//         {
-//             "image": "img/content.png",
-//             "title": "Row 1",
-//             "date": "16 June 2013",
-//             "tags": "tag1, tag2",
-//         },
-//     ];
-//     $scope.row2 = [
-//         {
-//             "image": "img/content.png",
-//             "title": "Row 2",
-//             "date": "16 June 2013",
-//             "tags": "tag1, tag2",
-//         },
-//         {
-//             "image": "img/content.png",
-//             "title": "Row 2",
-//             "date": "16 June 2013",
-//             "tags": "tag1, tag2",
-//         },
-//         {
-//             "image": "img/content.png",
-//             "title": "Row 2",
-//             "date": "16 June 2013",
-//             "tags": "tag1, tag2",
-//         },
-//     ];
-//     $scope.row3 = [
-//         {
-//             "image": "img/content.png",
-//             "title": "Row 3",
-//             "date": "16 June 2013",
-//             "tags": "tag1, tag2",
-//         },
-//         {
-//             "image": "img/content.png",
-//             "title": "Row 3",
-//             "date": "16 June 2013",
-//             "tags": "tag1, tag2",
-//         },
-//         {
-//             "image": "img/content.png",
-//             "title": "Row 3",
-//             "date": "16 June 2013",
-//             "tags": "tag1, tag2",
-//         },
-//     ];
-//     $scope.fname = "Drew";
-//     $scope.lname = "Ellis";
-//     $scope.occupation = "Graphic Designer";
-//     $scope.city = "Alexandria";
-//     $scope.state = "VA";
-//     $scope.phone = "(867)-555-5309";
-//     $scope.email = "email@website.com";
-//     $scope.skype = "skypeusername";
-
-//     $scope.openContentEditor = function(item) {
-//         var d = $dialog.dialog({dialogFade: true; resolve: {item: function() { return angular.copy(item); } }});
-//         d.open('partials/dialogs/content-detail.html', 'ContentDetailController');
-//     };
-// };
 
 app.controller('ContentDetailController', ['$scope','dialog', 'item', function($scope, dialog, item) {
     $scope.item = item;
