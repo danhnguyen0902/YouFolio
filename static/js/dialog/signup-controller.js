3dfa6628e979cd4c7b11e7d37dc7030dab3de696
Matt Flinn <mflinn@fool.com>
2013-07-09 23:27:47 -0400

    Wiring sign-up form..

--- static/js/dialog/signup-controller.js
+++ static/js/dialog/signup-controller.js
@@ -1,9 +1,12 @@
 app.controller('SignUpController', ['$scope','dialog', function($scope, dialog) {
     $scope.form = [];
     $scope.WelcomeMessage = "Get Started Now!";
+    $scope.SubmitText = "Let's Go!";
 
     $scope.signUp = function(form) {
-
+        console.log('Sign Up Form: FirstName = ' + form.firstName);
+        console.log('Sign Up Form: LastName = ' + form.lastName);
+        console.log('Sign Up Form: EmailName = ' + form.emailAddress);
     };
 
     $scope.close = function () {
