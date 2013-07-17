41ca3acaf5023a3b7fe8104487b0cf971050f8c6
Robert Wagner <rwwagner@vt.edu>
2013-07-17 10:29:53 -0400

    Search bar, buttons, and job postings for companies

--- static/js/controllers/companies.js
+++ static/js/controllers/companies.js
@@ -4,4 +4,21 @@ function CompaniesController($scope) {
     $scope.companyState = "VA";
     $scope.companyYear = 2009;
     $scope.employeeNumber = 27;
+    $scope.bio = "Hello we are a company! Work for us!";
+    $scope.jobs = [{
+        "title": "Junior Graphic Design/Intern",
+        "company": "Google Analytics",
+        "date": "12 May 2013",
+        "location": "Austin, TX",
+    }, {
+        "title": "Product Engineer/Specialist",
+        "company": "Company Name Goes Here",
+        "date": "16 June 2013",
+        "location": "The Middle of Nowhere, VA",
+    }, {
+        "title": "Job",
+        "company": "YouFolio",
+        "date": "16 June 2013",
+        "location": "Everywhere, USA",
+    }, ];
 };
